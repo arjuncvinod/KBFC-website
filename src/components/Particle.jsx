3dde601e00ed581e8ca49b43a969f1fb48b75066
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-11-12 23:15:01 +0530

    Particles created

---
+++ src/components/Particle.jsx
@@ -0,0 +1,25 @@
+import Particles from "react-tsparticles";
+import { useCallback } from "react";
+import particleconfig from "./config/particleconfig"
+// import { loadFull } from "tsparticles";
+import { loadSlim } from "tsparticles-slim";
+export default function Particle() {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    //  await loadFull(engine);
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
+  return (
+    <Particles
+      options={particleconfig}
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+    />
+  );
+}
