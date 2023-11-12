e4e7798624e4bf46909ecabb81b266c6c5201776
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-11-12 21:21:43 +0530

    Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css';
 import { Route,Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Squad from './pages/Squad';
+import Photos from './pages/Photos';
+import About from './pages/About';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/squad" element={<Squad />} />
+      <Route path="/photos" element={<Photos />} />
+      <Route path="/about" element={<About />}/>
     </Routes>
   );
 }
