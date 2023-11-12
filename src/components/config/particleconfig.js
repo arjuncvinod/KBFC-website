2761403f18263c75465b999fa68f16825e08f198
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-11-12 23:15:21 +0530

    partcile settings updated

---
+++ src/components/config/particleconfig.js
@@ -0,0 +1,92 @@
+const particleconfig = {
+  background: {
+    color: {
+      value: "#000",
+    },
+    image: "",
+    position: "",
+    repeat: "",
+    size: "",
+    opacity: 1,
+  },
+  backgroundMask: {
+    composite: "destination-out",
+    cover: {
+      color: {
+        value: "#fff",
+      },
+      opacity: 1,
+    },
+    enable: false,
+  },
+  defaultThemes: {},
+  delay: 0,
+  fullScreen: {
+    enable: true,
+    zIndex: -1,
+  },
+  fpsLimit: 144,
+  interactivity: {
+    events: {
+      onClick: {
+        enable: true,
+        mode: "repulse",
+      },
+      onHover: {
+        enable: true,
+        mode: "grab",
+      },
+      resize: true,
+    },
+    modes: {
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 200,
+        duration: 1,
+      },
+    },
+  },
+
+  particles: {
+    color: {
+      value: "#DAB38C",
+    },
+    links: {
+      color: "#fdf004",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outModes: {
+        default: "bounce",
+      },
+      random: false,
+      speed: 2,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      value: { min: 1, max: 5 },
+    },
+  },
+  detectRetina: true,
+};
+export default particleconfig;
