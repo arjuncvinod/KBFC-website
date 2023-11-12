3c71b141ba5e964ad08afc510a877d28ec12e639
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-11-12 21:23:50 +0530

    Home page updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
-import NavBar from "../components/NavBar"
+import NavBar from "../components/NavBar";
+import video from "../assets/videos/kbfc-bg.mp4";
+import { Link } from "react-router-dom";
 function Home() {
   return (
-    <main>
+    <main id="home">
       <NavBar />
+      <section className="hero">
+          <video autoPlay muted loop className="back-video">
+            <source src={video} type="video/mp4" />
+          </video>
+        <h1>Kerala Blasters</h1>
+        <Link to="/squad"><button>View more</button></Link>
+      </section>
     </main>
-  )
+  );
 }
 
-export default Home
+export default Home;
